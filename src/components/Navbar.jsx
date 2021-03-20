bc49bc19151926d4ec53944c99e998f320270d96
Houda BRIWA <43297808+houdabriwa@users.noreply.github.com>
2021-03-20 19:39:18 +0100

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -45,11 +45,11 @@ const Navigation = React.forwardRef((props, ref) => {
       <Navbar.Toggle aria-controls="basic-navbar-nav" className="toggler" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="mr-auto">
-          {/* {
+          { {
             <Nav.Link className="nav-link lead">
               <Link to={process.env.PUBLIC_URL + "/blog"}>Blog</Link>
             </Nav.Link>
-          } */}
+          } }
           {repos.show && (
             <Nav.Link
               className="nav-link lead"
