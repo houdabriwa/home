0ad77614a61f4b8d0e776331e1f46abfd98699a4
Houda BRIWA <43297808+houdabriwa@users.noreply.github.com>
2021-03-20 23:23:18 +0100

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -46,8 +46,11 @@ const Navigation = React.forwardRef((props, ref) => {
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="mr-auto">
           {blog.show && (
-             <Nav.Link className="nav-link lead">
-               <Link to={process.env.PUBLIC_URL + "/blog"}>Blog</Link>
+             <Nav.Link
+              className="nav-link lead"
+              href={process.env.PUBLIC_URL + "/#blog"}
+            >
+            Blog
              </Nav.Link>
            )}
            
