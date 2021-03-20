41423b61f480b1bdad4cd30073a3ed447c203ca0
Houda BRIWA <43297808+houdabriwa@users.noreply.github.com>
2021-03-20 18:07:16 +0100

    Update config.js

--- src/editable-stuff/config.js
+++ src/editable-stuff/config.js
@@ -54,7 +54,7 @@ const about = {
   imageLink: require("../editable-stuff/houda.jpg"),
   imageSize: 375,
   message:
-    "Data and knowledge state engineer and a recent Data science master graduate from polytechnic university of Madrid. I have a background in customer service, consulting and logistics. My main research interest lie in the application of machine learning and deep learning in the field of social science and neuroscience.",
+    "Data and knowledge state engineer and a recent Data science master graduate from polytechnic university of Madrid. I have a background in customer service, consulting and logistics. I'm interested in the application of machine learning and deep learning in the field of social science and ergonomics.",
   resume: require("../editable-stuff/resume.pdf"),
 };
 
@@ -73,8 +73,8 @@ const repos = {
 
 // Leadership SECTION
 const leadership = {
-  show: false,
-  heading: "Leadership",
+  show: true,
+  heading: "Publications",
   message:
     "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Vitae auctor eu augue ut lectus arcu bibendum at varius. Libero justo laoreet sit amet cursus sit amet. Imperdiet dui accumsan sit amet nulla facilisi morbi. At auctor urna nunc id. Iaculis urna id volutpat lacus laoreet non curabitur gravida. Et magnis dis parturient montes nascetur ridiculus mus mauris. In nisl nisi scelerisque eu ultrices vitae auctor. Mattis nunc sed blandit libero volutpat sed cras ornare. Pulvinar neque laoreet suspendisse interdum consectetur libero.",
   images: [
@@ -131,7 +131,7 @@ const getInTouch = {
 };
 
 const experiences = {
-  show: false,
+  show: true,
   heading: "Experiences",
   data: [
     {
