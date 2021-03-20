a8d2a59fe1fe634cc996e0b98f07644d043ce417
Houda BRIWA <43297808+houdabriwa@users.noreply.github.com>
2021-03-20 22:08:33 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -16,8 +16,8 @@ import Project from "./components/home/Project";
 import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
 import Skills from "./components/home/Skills";
-// import { Blog } from "./components/blog/Blog";
-// import BlogPost from "./components/blog/BlogPost";
+import { Blog } from "./components/blog/Blog";
+import BlogPost from "./components/blog/BlogPost";
 import GetInTouch from "./components/home/GetInTouch.jsx";
 import Leadership from "./components/home/Leadership.jsx";
 
