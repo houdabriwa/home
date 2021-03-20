24bfe7fcaa3a5af27c19961c35a94901fb0f2b05
Houda BRIWA <43297808+houdabriwa@users.noreply.github.com>
2021-03-20 16:46:46 +0100

    Updates

---
+++ precache-manifest.bfa73841771b36f9453e2243d236e782.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3588aa7c15e019ed1e3ae53e04a197ae",
+    "url": "/home/index.html"
+  },
+  {
+    "revision": "60133f33109290cb21e7",
+    "url": "/home/static/css/main.f5497eb8.chunk.css"
+  },
+  {
+    "revision": "94833f0d9aaa3be6210f",
+    "url": "/home/static/js/2.19e99e5d.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/home/static/js/2.19e99e5d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "60133f33109290cb21e7",
+    "url": "/home/static/js/main.bed04fe7.chunk.js"
+  },
+  {
+    "revision": "c0578b487099793a068c",
+    "url": "/home/static/js/runtime-main.3c590061.js"
+  },
+  {
+    "revision": "32f6b574eb11519020e24620a38bdf55",
+    "url": "/home/static/media/hashirshoaeb.32f6b574.png"
+  },
+  {
+    "revision": "5fa8698e6d0b4fdc95d5696c751e88fc",
+    "url": "/home/static/media/resume.5fa8698e.pdf"
+  }
+]);
