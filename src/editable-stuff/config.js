debcbdb3dbfa3685d103fe31fa1d0010a2a8461f
Houda BRIWA <43297808+houdabriwa@users.noreply.github.com>
2021-03-20 18:21:18 +0100

    Update config.js

--- src/editable-stuff/config.js
+++ src/editable-stuff/config.js
@@ -51,7 +51,7 @@ const mainBody = {
 const about = {
   show: true,
   heading: "About Me",
-  imageLink: require("../editable-stuff/houda.jpg"),
+  imageLink: require("../editable-stuff/houdabriwa.jpg"),
   imageSize: 375,
   message:
     "Data and knowledge state engineer and a recent Data science master graduate from polytechnic university of Madrid. I have a background in customer service, consulting and logistics. I'm interested in the application of machine learning and deep learning in the field of social science and ergonomics.",
@@ -79,7 +79,7 @@ const leadership = {
     "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Vitae auctor eu augue ut lectus arcu bibendum at varius. Libero justo laoreet sit amet cursus sit amet. Imperdiet dui accumsan sit amet nulla facilisi morbi. At auctor urna nunc id. Iaculis urna id volutpat lacus laoreet non curabitur gravida. Et magnis dis parturient montes nascetur ridiculus mus mauris. In nisl nisi scelerisque eu ultrices vitae auctor. Mattis nunc sed blandit libero volutpat sed cras ornare. Pulvinar neque laoreet suspendisse interdum consectetur libero.",
   images: [
     { 
-      img: require("../editable-stuff/houda.jpg"), 
+      img: require("../editable-stuff/houdabriwa.jpg"), 
       label: "First slide label", 
       paragraph: "Nulla vitae elit libero, a pharetra augue mollis interdum." 
     },
