15c5849d0886cacb664a09225aa090952631e71c
Houda BRIWA <43297808+houdabriwa@users.noreply.github.com>
2021-03-21 13:10:21 +0100

    Update LanguagesCard.jsx

--- src/components/home/LanguagesCard.jsx
+++ src/components/home/LanguagesCard.jsx
@@ -9,10 +9,10 @@ const LanguagesCard = ({data}) => {
         <Col lg="6">
             <div className="pb-5 text-center">
                 <img className=" bg-white mb-3" src={data.companylogo} alt=""/>
-                    <b>{data.role}<b>
+                     
                 <p className="lead">
-                
-                   
+                    {data.role} 
+                    <br/>
                     {data.prof}
                     <br/>
                     {data.message}
