c12657535413ff515d53bf4a3dd291da9c8c5404
Houda BRIWA <43297808+houdabriwa@users.noreply.github.com>
2022-01-25 19:19:24 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -40,7 +40,7 @@ const Home = React.forwardRef((props, ref) => {
           message={about.message}
           link={about.imageLink}
           imgSize={about.imageSize}
-          resume={about.resume}
+          CV={about.CV}
         />
       )}
       {
